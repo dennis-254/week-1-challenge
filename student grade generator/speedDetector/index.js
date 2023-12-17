67e8728127efe1b48affe17c0fd52882a324c13d
+dennis.mutua@student.moringaschool.com <dennis@DESKTOP-Q9A0DBN.localdomain>
2023-12-17 22:35:31 +0300

    commit

---
+++ student grade generator/speedDetector/index.js
@@ -0,0 +1,9 @@
+const speedX = document.getElementById("speed");
+const speedBTN = document.getElementById("speedBtn");
+
+
+//checking for speed parameters
+speedBTN.addEventListener("click", function())
+    const speed = speedX.value;
+    if(speed <= 70);
+    
